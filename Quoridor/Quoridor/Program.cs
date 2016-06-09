50acd94b3b0d4b67785c4ca05919fd99d5d621b9
PensiveBirch <tylerdsnook@gmail.com>
2016-06-09 00:16:18 -0700

    A fun method

--- Quoridor/Quoridor/Program.cs
+++ Quoridor/Quoridor/Program.cs
@@ -12,6 +12,13 @@ namespace Quoridor
         {
             Console.WriteLine("Hello World!");
             int firstVar = 1;
+            Add(firstVar, 89);
+            Console.ReadLine(); //Here just so we can see the output box before it disappears
+        }
+
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("Can you not add {0} + {1} on your own? Sheesh.\n\n\n(It's {2} by the way)", a, b, a+b);
         }
     }
 }
