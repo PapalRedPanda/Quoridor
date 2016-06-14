806cafbd892a90f3551cddb0260e9d6e3f91c552
PensiveBirch <tylerdsnook@gmail.com>
2016-06-13 18:00:28 -0700

    Commented and fixed up Space

--- Quoridor/Quoridor/Space.cs
+++ Quoridor/Quoridor/Space.cs
@@ -7,33 +7,42 @@ using System.Threading.Tasks;
 namespace Quoridor
 {
     /// <summary>
-    ///  B - not sure what you mean by space
-    ///  T- A space is one square on the board. The board can be an array of spaces
+    /// A space is one square on the board. The board is an array of spaces.
+    /// Each space has multiple characteristics as fields:
+    /// The ability to be occupied by a player's pawn, the ability to have walls to the east and south of it, and each
+    /// space can have the distinction of being on the bottommost row and/or the rightmost column of the board. Every space has 
+    /// a corresponding name.
+    /// 
+    /// Methods:
+    /// AddWall()
     /// </summary>
     class Space
     {
-        int occupied = 0; //0 if unoccupied, 1 if player 1, 2 if player 2, 3 if player 3, 4 if player 4
-        bool eastWall = false;
-        bool southWall = false; // what if the space has no south or east wall? just set it as true maybe?
-        bool southmostSpace;
-        bool eastmostSpace;
+        int occupied; //0 if unoccupied, 1 if player 1, 2 if player 2, 3 if player 3, 4 if player 4
+        bool eastWall = false;  //true if there is a wall in the interstice directly east of this space on the board
+        bool southWall = false; //true if there is a wall in the interstice directly south of this space on the board
+        bool southmostSpace; //true if this space is on the bottommost row on the board. If so, southWall will always be false
+        bool eastmostSpace; //true if this space is on the rightmost column on the board. If so, eastWall will always be false
         string name; //ex: e4 (it's the space's notation)
 
-        public Space(bool east = false, bool south = false)
+        //A basic call to the constructor with no arguments will result in a space that isn't on the bottommost row or rightmost collumn
+        //and the space will be unoccupied without walls to the south or east of it
+        public Space(bool east = false, bool south = false, int occupied = 0)
         {
             eastmostSpace = east;
             southmostSpace = south;
+            this.occupied = occupied;
         }
 
         //Only adds a wall if it can (it won't add a wall south of a space on the bottom row)
         //southPosition: True: add south wall; False: add east wall
         public void AddWall(bool southPosition)
         {
-            if (southWall && !southmostSpace)
+            if (southPosition && !southmostSpace)
             {
                 southWall = true;
             }
-            else if (!southWall && !eastmostSpace)
+            else if (!southPosition && !eastmostSpace)
             {
                 eastWall = true;
             }
